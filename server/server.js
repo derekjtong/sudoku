51aa802c1bbe3f6273e574ca8787e6e859715086
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-10 02:54:50 -0500

    Updated server start console message

--- server/server.js
+++ server/server.js
@@ -7,5 +7,5 @@ const { ppid } = require("process");
 app.use(express.static(path.join(__dirname, "/dist")));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
