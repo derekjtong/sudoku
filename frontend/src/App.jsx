5110a8ed03aa7bb041bc8cb6aef9ada009aed4a5
mohammadmurtuza <62512039+mohammadmurtuza@users.noreply.github.com>
2023-11-11 17:27:57 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import Board from "./components/Board";
+
 function Home() {
-  return <div>SUDOKU</div>;
+  return (
+    <div>
+      <div className="text-4xl">SUDOKU</div>
+      <Board />
+    </div>
+  );
 }
 
 export default Home;
