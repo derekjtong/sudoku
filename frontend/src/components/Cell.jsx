477360e63990e428c3cbd2a81d8227614f0fa0d7
mohammadmurtuza <62512039+mohammadmurtuza@users.noreply.github.com>
2023-11-11 17:28:00 -0500

    Create Cell.jsx

---
+++ frontend/src/components/Cell.jsx
@@ -0,0 +1,17 @@
+// Cell.js
+import React from "react";
+
+function Cell({ row, col, value, onCellClick, onChange, isSelected }) {
+  return (
+    <input
+      type="text"
+      className={`m-1 h-10 w-10 border border-gray-800 text-center ${isSelected ? "bg-yellow-200" : ""}`}
+      value={value}
+      onChange={onChange}
+      onClick={() => onCellClick(row, col)}
+      maxLength="1"
+    />
+  );
+}
+
+export default Cell;
