b2493f5545a1ebb1b0c593b6eaa8f5af1d14a148
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-11 18:35:38 -0500

    Imported Board component into content

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,8 +1,10 @@
+import Board from "./Board";
+
 function Content() {
   return (
     <div className="flex min-h-screen">
       <div className="container mx-auto mt-20 border border-black">
-        <div>Main Content</div>
+        <Board />
       </div>
     </div>
   );
