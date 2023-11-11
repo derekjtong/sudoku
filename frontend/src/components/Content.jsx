5259ed384a0d624dca5ccdc72fe4f6f31d2923f9
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-11 18:32:46 -0500

    Added Content placeholder component

---
+++ frontend/src/components/Content.jsx
@@ -0,0 +1,11 @@
+function Content() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="container mx-auto mt-20 border border-black">
+        <div>Main Content</div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
