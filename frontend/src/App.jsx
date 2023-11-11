4e7ca43d3959a48644da85832c20a2267ed738f4
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-11 16:25:57 -0500

    Added buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import Buttons from "./components/Buttons";
 import Logo from "./components/Logo";
-
-
 import Nav from "./components/Nav";
 
 function Home() {
   return (
     <>
+      <Nav />
       <Logo />
       <div>Hello</div>
       <Buttons />
