dd7dba362a7d3f6669154c8b087d1989894ea2ba
Chenyu Hao <m568912049@gmail.com>
2023-11-11 16:20:13 -0500

    added NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import Nav from "./components/Nav";
+
 function Home() {
-  return <div>SUDOKU</div>;
+  return (
+    <>
+      <Nav />
+      <div>Hello</div>
+    </>
+
+  )
+
+
 }
 
 export default Home;
