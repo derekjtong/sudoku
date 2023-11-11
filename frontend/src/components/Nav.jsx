aa5880b7597da9ef3f1e465cfac7defc05271b46
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-11 18:33:08 -0500

    Added Logo component to Navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,30 +1,33 @@
+import Logo from "./Logo";
 
 const Nav = () => {
-    return (
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                {/* Logo or brand */}
-                <div className="text-white text-lg font-bold">SUDOKU</div>
+  return (
+    <nav className="fixed top-0 z-50 w-full bg-gray-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo or brand */}
+        <Logo />
 
-                {/* Navigation links */}
-                <ul className="flex space-x-4">
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300">New Game:</a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300">Easy:4*4</a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300">Middle:6*6</a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300">Diffcult:9*9</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+        {/* Navigation links */}
+        <ul className="flex space-x-4 text-2xl">
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              New Game
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              4 x 4
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              9 x 9
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
-
 
 export default Nav;
