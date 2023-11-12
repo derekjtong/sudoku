b8b7cb93d2b09de2520e72c68e98e237f2c9f437
kaylalaufer <klaufer1@jhu.edu>
2023-11-12 00:21:20 -0500

    added button labels

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -1,5 +1,13 @@
 function Buttons() {
-    return <div className="text-3x1">Undo</div>;
+    return (
+        <>
+        <div className="text-3x1">Undo</div>
+        <div className="text-3x1">Erase</div>
+        <div className="text-3x1">Notes</div>
+        <div className="text-3x1">Hint</div>
+        </>
+    );
+
 }
 
 export default Buttons;
