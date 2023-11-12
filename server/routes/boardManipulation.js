254e8a9f91575b0244a90279d9ec50733526f011
Safalgautam3636 <Safalgautam3636@gmail.com>
2023-11-12 15:19:01 -0500

    set-up api for add element

--- server/routes/boardManipulation.js
+++ server/routes/boardManipulation.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import addElementIntoBoard from '../controllers/addElementIntoBoard';
+import {addElementIntoBoard} from '../controllers/addElementIntoBoard.js';
 const router = express.Router();
 
 router.post('/addelement', addElementIntoBoard);
-
+//router.post("/deleteelement", addElementIntoBoard);
 export default router;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import {generateBoard}  from './helpers/generateBoard.js';
-import {boardManipulationRoute} from './routes/boardManipulation.js';
-
+import boardManipulationRoute from './routes/boardManipulation.js';
+import getBoard from './routes/getBoard.js'
 
 
 const app = express();
@@ -12,13 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // all routes called here
 app.use("/api", boardManipulationRoute);
+app.use("/api", getBoard);
 // app.use('/api',require('./routes/boardManipulation'));
 
 
 
 
-app.use(express.static(path.join(__dirname, "/dist")));
-port = 9090;
+
+const port = 9090;
 app.listen(port, () => {
   console.log(`Started server on http://localhost:${port}/`);
 });
