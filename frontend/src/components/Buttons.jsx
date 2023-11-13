c7f2fe3d63217625bc5355edf82a03e6bb5413ba
kaylalaufer <klaufer1@jhu.edu>
2023-11-13 16:35:01 -0500

    Added Buttons (undo, note, hint, check board). Added Buttons.css file for button styling

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -1,13 +1,24 @@
-function Buttons() {
-    return (
-        <>
-        <div className="text-3x1">Undo</div>
-        <div className="text-3x1">Erase</div>
-        <div className="text-3x1">Notes</div>
-        <div className="text-3x1">Hint</div>
-        </>
-    );
+import React, { useState } from 'react';
+import './Buttons.css';
 
-}
+const Buttons = ({ onUndo, onErase, onToggleNotes, onGetHint }) => {
+  const [notesOn, setNotesOn] = useState(false);
+
+  const toggleNotes = () => {
+    setNotesOn(!notesOn);
+    onToggleNotes(!notesOn); // Call the provided prop function with the updated state
+  };
+
+  return (
+    <div className="buttons-container">
+      <button onClick={onUndo}>Undo</button>
+      <button onClick={toggleNotes}>
+        {notesOn ? 'Notes On' : 'Notes Off'}
+      </button>
+      <button onClick={onGetHint}>Hint</button>
+      <button onClick={onErase}>Check Board</button>
+    </div>
+  );
+};
 
 export default Buttons;
