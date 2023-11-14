85a537e702f0ba07f70827d5873973f109fcdb9e
kaylalaufer <klaufer1@jhu.edu>
2023-11-14 09:38:09 -0500

    Updated Buttons.jsx and README

--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './Buttons.css';
 
 const Buttons = ({ onUndo, onErase, onToggleNotes, onGetHint }) => {
   const [notesOn, setNotesOn] = useState(false);
@@ -10,13 +9,13 @@ const Buttons = ({ onUndo, onErase, onToggleNotes, onGetHint }) => {
   };
 
   return (
-    <div className="buttons-container">
-      <button onClick={onUndo}>Undo</button>
-      <button onClick={toggleNotes}>
+    <div className="fixed bottom-5 w-full bg-gray-800 p-4 flex justify-around">
+      <button className="text-white" onClick={onUndo}>Undo</button>
+      <button className="text-white" onClick={toggleNotes}>
         {notesOn ? 'Notes On' : 'Notes Off'}
       </button>
-      <button onClick={onGetHint}>Hint</button>
-      <button onClick={onErase}>Check Board</button>
+      <button className="text-white" onClick={onGetHint}>Hint</button>
+      <button className="text-white" onClick={onErase}>Check Board</button>
     </div>
   );
 };
