fb33de7883c1efede4addfc9b0a0a1b68c995b7c
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-30 14:59:42 -0500

    Added server IP address to console

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-
-import express from 'express';
-import bodyParser from 'body-parser'
-import boardManipulationRoute from './routes/boardManipulation.js';
+import express from "express";
+import bodyParser from "body-parser";
+import boardManipulationRoute from "./routes/boardManipulation.js";
 import boardRouter from "./routes/getBoard.js";
-import makeDB from './database/database.js';
-import gamesRouter from './routes/games.js';
-
+import makeDB from "./database/database.js";
+import gamesRouter from "./routes/games.js";
 
 const app = express();
 app.use(express.json());
@@ -17,19 +15,13 @@ app.use("/api", boardRouter);
 app.use("/api", gamesRouter);
 // app.use('/api',require('./routes/boardManipulation'));
 
-
-
-
-
 const port = 9090;
 
 app.listen(port, (err) => {
   makeDB();
   if (!err) {
-    console.log('Server is up and running..');
-  }
-  else {
+    console.log(`Server is running on http://localhost:${port}/`);
+  } else {
     console.log(err);
   }
-  
-})
+});
