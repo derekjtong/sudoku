d56e002eab8ff346346d540cf9b1ce3bd824c83d
And1Jay <147187561+And1Jay@users.noreply.github.com>
2023-12-01 19:52:08 -0500

    delete 16*16 route

--- server/routes/getBoard.js
+++ server/routes/getBoard.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { getFourBoard } from '../controllers/getFourBoard.js';
 import { getNineBoard } from '../controllers/getNineBoard.js';
-import { getSixteenBoard } from '../controllers/getSixteenBoard.js';
 
 const router = express.Router();
 
@@ -10,6 +9,5 @@ const router = express.Router();
 // })
 router.get("/newboard/:id", getFourBoard);
 router.get("/newboard/:id", getNineBoard);
-router.get("/newboard/:id", getSixteenBoard);
 
 export default boardRouter;
