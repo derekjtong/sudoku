22a1e75d87803d3785f4498425991a168df319d1
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-02 03:13:03 -0500

    Change difficulty and size

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Buttons from "./components/shared/Buttons";
 import Content from "./components/shared/Content";
 import Navbar from "./components/shared/Navbar";
 
 function Home() {
+  const [boardDimension, setBoardDimension] = useState(9);
+  const [difficulty, setDifficulty] = useState(1);
   return (
     <>
-      <Navbar />
-      <Content />
+      <Navbar setBoardDimension={setBoardDimension} setDifficulty={setDifficulty} />
+      <Content boardDimension={boardDimension} difficulty={difficulty} />
       <Buttons />
     </>
   );
