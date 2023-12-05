30d06660350c191cac59c634cf8c36d8aaabff2a
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-05 15:35:41 -0500

    Remove white space

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-import express, { application } from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" }); // Loads environment variables from .env.local file
+import express from "express";
 import bodyParser from "body-parser";
+import { generateBoard } from "./helpers/generateBoard.js";
 import boardManipulationRoute from "./routes/boardManipulation.js";
-import boardRouter from "./routes/getBoard.js";
-import makeDB from "./database/database.js";
-import gamesRouter from "./routes/games.js";
-import cors from "cors";
+import getBoard from "./routes/getBoard.js";
+import { connectToServer } from "./database/database.js";
 
 const app = express();
 const port = 9090;
@@ -21,11 +22,15 @@ app.use("/api", boardRouter);
 app.use("/api", gamesRouter);
 // app.use('/api',require('./routes/boardManipulation'));
 
-app.listen(port, (err) => {
-  makeDB();
-  if (!err) {
-    console.log(`Server is running on http://localhost:${port}/`);
-  } else {
-    console.log(err);
+const port = 9090;
+
+connectToServer((error) => {
+  if (error) {
+    console.error("Failed to connect to the database!", error);
+    process.exit();
   }
+
+  app.listen(port, () => {
+    console.log(`Started server on http://localhost:${port}/`);
+  });
 });
