a795e58027c59673681f7a1ba18f32afa8b9911b
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-05 15:33:05 -0500

    Remove deprecated flags

--- server/database/database.js
+++ server/database/database.js
@@ -1,24 +1,24 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 let dbConnection;
 
 export const connectToServer = (callback) => {
-    client.connect((err, db) => {
-        if (err) {
-            console.error("Could not connect to MongoDB", err);
-            return callback(err);
-        }
-        dbConnection = db.db('sudokuDB');
-        console.log("Successfully connected to MongoDB.");
-        return callback(null);
-    });
+  client.connect((err, db) => {
+    if (err) {
+      console.error("Could not connect to MongoDB", err);
+      return callback(err);
+    }
+    dbConnection = db.db("sudokuDB");
+    console.log("Successfully connected to MongoDB.");
+    return callback(null);
+  });
 };
 
 export const getDb = () => {
-    return dbConnection;
+  return dbConnection;
 };
