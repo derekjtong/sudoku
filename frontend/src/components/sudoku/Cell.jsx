3da57113ec6d17f05204d7eff1e810340f3aaba8
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-05 17:01:10 -0500

    Change cursor type

--- frontend/src/components/sudoku/Cell.jsx
+++ frontend/src/components/sudoku/Cell.jsx
@@ -4,7 +4,7 @@ function Cell({ row, col, value, onCellClick, onChange, isSelected, isPrimarySel
   return (
     <input
       type="text"
-      className={`m-0 h-14  w-14 cursor-pointer border border-gray-800 text-center caret-transparent ${isSelected ? "bg-gray-200" : ""} ${
+      className={`m-0 h-14  w-14 cursor-default border border-gray-800 text-center caret-transparent ${isSelected ? "bg-gray-200" : ""} ${
         isPrimarySelected ? "bg-gray-400 text-white" : ""
       }`}
       value={value}
