1477c78e56fa03650cb9fd63e7017e2b5784ae8f
Vincentlove18 <91401704+Vincentlove18@users.noreply.github.com>
2023-12-05 20:42:30 -0500

    Modify getUndo.js

--- frontend/src/api/getUndo.js
+++ frontend/src/api/getUndo.js
@@ -3,35 +3,18 @@ import axios from "axios";
 const BASE_URL = "http://localhost:9090/api";
 
 
-export const getUndoFour = () => {
+export const getUndoFour = (GameID) => {
     return axios
-      .get(`${BASE_URL}/undo/4`)
+      .get(`${BASE_URL}/undo/${GameID}`)
       .then((response) => response.data)
       .catch((error) => {
         throw new Error(`Error fetching undo: ${error.message}`);
       });
   };
 
-  export const getUndoUntilCorrectFour = () => {
+  export const getUndoUntilCorrectFour = (GameID) => {
     return axios
-      .get(`${BASE_URL}/undountilcorrect/4`)
-      .then((response) => response.data)
-      .catch((error) => {
-        throw new Error(`Error fetching undo until correct: ${error.message}`);
-      });
-  };
-  export const getUndoNine = () => {
-    return axios
-      .get(`${BASE_URL}/undo/9`)
-      .then((response) => response.data)
-      .catch((error) => {
-        throw new Error(`Error fetching undo: ${error.message}`);
-      });
-  };
-
-  export const getUndoUntilCorrectNine = () => {
-    return axios
-      .get(`${BASE_URL}/undountilcorrect/9`)
+      .get(`${BASE_URL}/undountilcorrect/${GameID}`)
       .then((response) => response.data)
       .catch((error) => {
         throw new Error(`Error fetching undo until correct: ${error.message}`);
