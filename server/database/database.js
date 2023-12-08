b12cde0ba8165d53e3be43913a5d12e9a5c34248
And1Jay <147187561+And1Jay@users.noreply.github.com>
2023-12-08 18:30:27 -0500

    connect to database

--- server/database/database.js
+++ server/database/database.js
@@ -1,24 +1,40 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
+import mongoose from "mongoose";
+const url = process.env.MONGODB_URI;
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+function makeDB() {
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log("Conected to db..");
+    })
+    .catch((err) => {
+      console.error("Error connecting to database: " + err.message);
+    });
+}
+export default makeDB;
+// import dotenv from "dotenv";
+// dotenv.config();
+// import { MongoClient } from "mongodb";
 
-let dbConnection;
+// const uri = process.env.MONGODB_URI;
+// const client = new MongoClient(uri);
 
-export const connectToServer = (callback) => {
-  client.connect((err, db) => {
-    if (err) {
-      console.error("Could not connect to MongoDB", err);
-      return callback(err);
-    }
-    dbConnection = db.db("sudokuDB");
-    console.log("Successfully connected to MongoDB.");
-    return callback(null);
-  });
-};
+// let dbConnection;
 
-export const getDb = () => {
-  return dbConnection;
-};
+// export const connectToServer = (callback) => {
+//   client.connect((err, db) => {
+//     if (err) {
+//       console.error("Could not connect to MongoDB", err);
+//       return callback(err);
+//     }
+//     dbConnection = db.db("sudokuDB");
+//     console.log("Successfully connected to MongoDB.");
+//     return callback(null);
+//   });
+// };
+
+// export const getDb = () => {
+//   return dbConnection;
+// };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
+// dotenv.config();
+// console.log(process.env.MONGODB_URI);
+//console.log(process.env); // This will print all environment variables
 import express, { application } from "express";
 import bodyParser from "body-parser";
 import boardManipulationRoute from "./routes/boardManipulation.js";
 import boardRouter from "./routes/getBoard.js";
-import { connectToServer, getDb } from "./database/database.js";
-//import makeDB from "./database/database.js";
+import makeDB from "./database/database.js";
 import gamesRouter from "./routes/games.js";
 import cors from "cors";
 
@@ -23,10 +26,46 @@ app.use("/api", gamesRouter);
 // app.use('/api',require('./routes/boardManipulation'));
 
 app.listen(port, (err) => {
-  //makeDB();
+  makeDB();
   if (!err) {
     console.log(`Server is running on http://localhost:${port}/`);
   } else {
     console.log(err);
-}
+  }
 });
+
+// import express, { application } from "express";
+// import bodyParser from "body-parser";
+// import boardManipulationRoute from "./routes/boardManipulation.js";
+// import boardRouter from "./routes/getBoard.js";
+// //import { connectToServer, getDb } from "./database/database.js";
+// //import makeDB from "./database/database.js";
+// import gamesRouter from "./routes/games.js";
+// import cors from "cors";
+
+// const app = express();
+// const port = 9090;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(express.static("dist"));
+
+// // all routes called here
+// app.use("/api", boardManipulationRoute);
+// app.use("/api", boardRouter);
+// app.use("/api", gamesRouter);
+// // app.use('/api',require('./routes/boardManipulation'));
+
+// connectToServer((error) => {
+//   if (error) {
+//     console.error("Failed to connect to the database!", error);
+//     process.exit(1); // Exit process with failure code
+//   }
+
+//   // Only start the server if there is no error
+//   app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}/`);
+//   });
+// });
