0569de58062b0c54c257e5d6111ecd8e24295c51
Safalgautam3636 <Safalgautam3636@gmail.com>
2023-12-12 01:19:51 -0500

    update schema and update few logic

--- server/helpers/createGame.js
+++ server/helpers/createGame.js
@@ -1,15 +1,39 @@
 import Game from "../database/gameSchema.js";
 const createGame = async (problemBoard, solutionBoard, dimension, stack) => {
   try {
+    // the problem board cell is the object that has two things one is the value and notes(size*size of the board)
+    //[[1,2,3,4],[2,3,4,1],[3,4,1,2],[4,1,2,3]]=>
+    // [
+    // [ { 1, [ [], [], [], [] ]}, { 2, [ [], [], [], [] ]}, { 3, [ [], [], [], [] ]}, { 4, [ [], [], [], [] ]} ],
+    // [ { 2, [ [], [], [], [] ]}, { 3, [ [], [], [], [] ]}, { 4, [ [], [], [], [] ]}, { 1, [ [], [], [], [] ]} ],
+    // [ { 3, [ [], [], [], [] ]}, { 4, [ [], [], [], [] ]}, { 2, [ [], [], [], [] ]}, { 2, [ [], [], [], [] ]} ],
+    // [ { 4, [ [], [], [], [] ]}, { 1, [ [], [], [], [] ]}, { 1, [ [], [], [], [] ]}, { 3, [ [], [], [], [] ]} ]
+    //]
+    let notes = [];
+    let updatedProblemBoard=[]
+
+    for (let i = 0; i < dimension; i++){
+      notes.push([]);
+    }
+    for (let i = 0; i < dimension; i++){
+      let initiateRow=[]
+      for (let j = 0; j < dimension; j++){
+        initiateRow.push({
+          value: problemBoard[ i ][ j ],
+          notes: notes
+        });
+      }
+      updatedProblemBoard.push(initiateRow);
+    }
     const newGame = new Game({
-      problemBoard,
+      problemBoard:updatedProblemBoard,
       solutionBoard,
       dimension,
       stack,
+      noteMode:false
     });
 
     const savedGame = await newGame.save();
-    console.log("Game saved:", savedGame);
     return savedGame;
   } catch (error) {
     console.error("Error saving game:", error);
