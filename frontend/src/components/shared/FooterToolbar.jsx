918503b77030c00198d7aa2fc07e8d51fb7ca279
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-11 22:00:29 -0500

    Edit api

--- frontend/src/components/shared/FooterToolbar.jsx
+++ frontend/src/components/shared/FooterToolbar.jsx
@@ -12,40 +12,22 @@ const FooterToolbar = ({ onToggleNotes, currentGameId, setCurrentGameId }) => {
 
   return (
     <div className="fixed bottom-0 flex w-full justify-around bg-gray-800">
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={() => undo(currentGameId, setCurrentGameId)}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={() => undo(currentGameId, setCurrentGameId)}>
         Undo
       </button>
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={() => undoUntilCorrect(currentGameId, setCurrentGameId)}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={() => undoUntilCorrect(currentGameId, setCurrentGameId)}>
         Undo Until Correct
       </button>
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={toggleNotes}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={toggleNotes}>
         {notesOn ? "Notes On" : "Notes Off"}
       </button>
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={() => getRandomHint(currentGameId)}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={() => getRandomHint(currentGameId)}>
         Random Hint
       </button>
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={() => getSpecificHint(currentGameId)}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={() => getSpecificHint(currentGameId)}>
         Specific Hint
       </button>
-      <button
-        className="w-full p-4 text-white hover:bg-gray-900"
-        onClick={() => correctSoFar(currentGameId)}
-      >
+      <button className="w-full p-4 text-white hover:bg-gray-900" onClick={() => correctSoFar(currentGameId)}>
         Check Board
       </button>
     </div>
