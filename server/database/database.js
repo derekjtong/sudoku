971177322b538510a327469028927ec7e814c365
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-11 22:30:11 -0500

    Add fallback connection database URI

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/sudoku";
 
 function makeDB() {
   mongoose
