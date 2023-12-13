7321256974f46bb06732a9c788431fe0aa993788
Safalgautam3636 <Safalgautam3636@gmail.com>
2023-12-13 01:09:42 -0500

    fix undo and undo untilcorrect and hint functions

---
+++ server/controllers/getSpecificHint.js
@@ -0,0 +1,36 @@
+import Game from "../database/gameSchema.js";
+import { ObjectId } from "mongodb";
+import doubleStack from "./doubleStack.js";
+//{suggestedMove}
+//{0,0}
+//indexes: {row: col:}
+
+const getSpecificHint =  (board, row, col) => {
+  
+  try {
+    let solution = board[ "solutionBoard" ];
+    const element = solution[ row ][ col ];
+    return { suggestedMove: { row, col, num: element } };
+  } catch (err) {
+    return { message: "Internal Server Error", err };
+  }
+};
+const callSpecificHint = async (req, res) => {
+  const gameId = new ObjectId(req.params.id);
+  let board = await Game.findOne({ _id: gameId });
+  let row = parseInt(req.body.row);
+  let col = parseInt(req.body.col);
+  const {suggestedMove} = getSpecificHint(board, row, col);
+  let problemBoard = board[ "problemBoard" ];
+  let gameStack = board[ "stack" ];
+
+  problemBoard = await doubleStack(suggestedMove, board, gameId, gameStack);
+
+  return res.json({
+    suggestedMove,
+    updatedBoard: problemBoard,
+  });
+};
+
+
+export default callSpecificHint;
--- server/controllers/undoUntilCorrect.js
+++ server/controllers/undoUntilCorrect.js
@@ -1,23 +1,26 @@
 import Game from "../database/gameSchema.js";
 import stack from "../helpers/stack.js";
+import {ObjectId} from "mongodb";
 // board which is the latest one
 const undoUntilCorrect = async (req, res) => {
   try {
     const gameId = new ObjectId(req.params.id);
+    console.log(gameId)
     let stackDb = await Game.findOne({ _id: gameId });
     stackDb = stackDb["stack"];
-    if (stackDb.size() === 0) {
+    if (stackDb.length === 0) {
       return res.status(400).json({ error: "Stack is empty." });
     }
 
     let correct;
-    let { grid, booleanValue } = stackDb.peek();
+    let { grid, booleanValue } = stackDb[stackDb.length-1];
 
     // Undo changes until a correct state is reached
-    while (!booleanValue && stackDb.size() > 0) {
+    while (!booleanValue && stackDb.length > 0) {
       correct = stackDb.pop();
       ({ grid, booleanValue } = correct);
     }
+    stackDb.push({ grid, booleanValue });
     let updateGame = await Game.updateOne({ _id: gameId }, { problemBoard: grid, stack: stackDb });
 
     return res.json({
@@ -26,7 +29,7 @@ const undoUntilCorrect = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: "Internal Server Error", error });
+    return res.status(500).json({ message: "Internal Server Error", err });
   }
 };
 
