3f52cd49cf3257a64827aa1e65b7f5afc5a7e511
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-13 05:37:10 -0500

    Remove comment

--- frontend/src/components/shared/AdminDialog.jsx
+++ frontend/src/components/shared/AdminDialog.jsx
@@ -14,7 +14,6 @@ const AdminDialog = ({ onClose, currentGameId }) => {
     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50">
       <div className="rounded-md bg-white p-8 shadow-md">
         <h2 className="mb-4 text-lg font-semibold">Admin Commands</h2>
-        {/* Add other admin commands here */}
         <button className="mb-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600" onClick={handleResetGame}>
           Reset Game
         </button>
