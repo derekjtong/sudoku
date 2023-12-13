40fb1de94f90f0ae408e933e0a0daf4fbcda0191
Safalgautam3636 <Safalgautam3636@gmail.com>
2023-12-13 00:04:30 -0500

    fix format

--- server/controllers/getRandomHint.js
+++ server/controllers/getRandomHint.js
@@ -7,27 +7,27 @@ import doubleStack from "./doubleStack.js";
 // TODO: - undo undountilcorrect and stack remove elements check properly
 
 const checkIfValidInDB = (board, row, col, num) => {
-  //check it from the db and see the element is valid or not 
-  const solutionBoard = board[ "solutionBoard" ];
-  if (solutionBoard[ row ][ col ] === num) {
+  //check it from the db and see the element is valid or not
+  const solutionBoard = board["solutionBoard"];
+  if (solutionBoard[row][col] === num) {
     return true;
   }
   return false;
-}
-const getRandomHint =  (board) => {
+};
+const getRandomHint = (board) => {
   try {
-    // before getting the hint check if the cell(x,y) already has the value 
+    // before getting the hint check if the cell(x,y) already has the value
     // if already has the value then i have remove all the entries that has been adding the value of cell(x,y) from the stack
     //update the stack using two stacks
     // REF: prof lecture notes
 
     // console.log(board["problemBoard"])
-    const gridSize = board[ "problemBoard" ].length;
+    const gridSize = board["problemBoard"].length;
     const badCells = [];
     for (let row = 0; row < gridSize; row++) {
       for (let col = 0; col < gridSize; col++) {
-        if (board[ "problemBoard" ][ row ][ col ].value !== -1) {
-          const num = board[ "problemBoard" ][ row ][ col ].value;
+        if (board["problemBoard"][row][col].value !== -1) {
+          const num = board["problemBoard"][row][col].value;
           if (!checkIfValidInDB(board, row, col, num)) {
             badCells.push({ row, col });
           }
@@ -36,56 +36,52 @@ const getRandomHint =  (board) => {
     }
     if (badCells.length > 0) {
       const randomIndex = Math.floor(Math.random() * badCells.length);
-      const { row, col } = badCells[ randomIndex ];
-      return { suggestedMove: { row, col, num: board[ "solutionBoard" ][ row ][ col ] } };
+      const { row, col } = badCells[randomIndex];
+      return { suggestedMove: { row, col, num: board["solutionBoard"][row][col] } };
     }
-    
+
     // incase we dont have the board is empty
     for (let row = 0; row < gridSize; row++) {
       for (let col = 0; col < gridSize; col++) {
-        if (board[ "problemBoard" ][ row ][ col ].value === -1) {
-          
-            return { suggestedMove: { row, col, num: board["solutionBoard"][row][col] } };
+        if (board["problemBoard"][row][col].value === -1) {
+          return { suggestedMove: { row, col, num: board["solutionBoard"][row][col] } };
         }
       }
     }
-    console.log('this is no hint');
+    console.log("this is no hint");
     return { message: "No hint available", suggestedMove: null };
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return { message: "Internal Server Error", error: err };
   }
 };
-
-
 
 const callRandomHint = async (req, res) => {
   const gameId = new ObjectId(req.params.id);
   let board = await Game.findOne({ _id: gameId });
   const { suggestedMove } = getRandomHint(board);
-  console.log(suggestedMove)
-  let problemBoard = board[ "problemBoard" ];
+  console.log(suggestedMove);
+  let problemBoard = board["problemBoard"];
   console.log("problem", problemBoard);
   let gameStack = board["stack"];
-  problemBoard=await doubleStack(suggestedMove,board,gameId,gameStack)
-  
- 
+  problemBoard = await doubleStack(suggestedMove, board, gameId, gameStack);
+
   // const row = suggestedMove.row;
   // const col = suggestedMove.col;
   // const element = suggestedMove[ "num" ];
   // const noteMode = board.noteMode;
   // if (!checkIfCellHasValue(board, row, col)) {
   //   //save it into the db and return json nothing much
-    
+
   //   problemBoard[ row ][ col ].value = element;
-    
+
   //   gameStack.push({ grid: problemBoard, booleanValue: checkIfValid(problemBoard) });
   //   //updatestack
-    
+
   //   await updateGame(problemBoard, gameId, gameStack, noteMode);
   //   return res.json({suggestedMove,updatedBoard:problemBoard})
   // }
-  
+
   // // stack-> [a,b,c]
   // // pop()= c ->[a,b]
   // //push(d)->[a,b,d]
@@ -107,7 +103,7 @@ const callRandomHint = async (req, res) => {
   //   }
   // }
   // //
-  
+
   // problemBoard[ row ][ col ].value = element;
   // gameStack.push({
   //   grid:problemBoard,
@@ -116,8 +112,8 @@ const callRandomHint = async (req, res) => {
   // updateGame(problemBoard, gameId, gameStack, noteMode);
   return res.json({
     suggestedMove,
-    updatedBoard:problemBoard
-  })
-}
+    updatedBoard: problemBoard,
+  });
+};
 
 export default callRandomHint;
